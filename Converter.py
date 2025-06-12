5a0776a09a2da54febef0eb8858a8dc4e916b7c5
Blake Newton <bnewton92.bn@gmail.com>
2025-06-12 09:14:24 -0400

    updated variables

--- Converter.py
+++ Converter.py
@@ -1,5 +1,10 @@
 from moviepy import VideoFileClip
 
+# Assign filepaths to the below variables
+input_file_path = None
+output_file_path = None
+
+
 def avi_to_mp4(input_path, output_path):
     """
     Converts an AVI video file to MP4 format.
@@ -12,5 +17,6 @@ def avi_to_mp4(input_path, output_path):
     clip.write_videofile(output_path, codec="libx264", audio_codec="aac")
     clip.close()
 
+
 if __name__ == "__main__":
-    avi_to_mp4("I:\\39 Blake Newton\Other\\Vince's Mirra Backrest Test\\04.02\\04.02 Crack Formation.AVI", "04.02 Crack Formation.mp4")
+    avi_to_mp4(input_file_path, output_file_path)
